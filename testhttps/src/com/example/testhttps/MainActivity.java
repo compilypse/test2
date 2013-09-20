cbe2cad7ca3dad1de9e9d5881b2fb8bf1f5a628a
compilypse <compilypse@gmail.com>
2013-09-19 22:28:05 -0400

    fdtrdrd

--- testhttps/src/com/example/testhttps/MainActivity.java
+++ testhttps/src/com/example/testhttps/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		int i = 2;
 		setContentView(R.layout.activity_main);
 	}
 
